40ab1cc56741aef5694cbbe302f22df245fac51c
CptBaroud <gurvan.seveno@gmail.com>
2018-05-28 15:49:22 +0200

    Add Level

---
+++ lorann/model/src/main/java/model/element/Level.java
@@ -0,0 +1,12 @@
+package model.element;
+
+import model.Element;
+
+public class Level {
+
+    private Element[][]elements = new Element[12][20];
+
+    public Level(Element[][] elements) {
+        this.elements = elements;
+    }
+}
