1d54bd32b306526a8c14f1273d1bc74bfa41b2e1
CptBaroud <gurvan.seveno@gmail.com>
2018-05-28 15:56:22 +0200

    Modify Level

--- lorann/model/src/main/java/model/element/Level.java
+++ lorann/model/src/main/java/model/element/Level.java
@@ -6,8 +6,7 @@ public class Level {
 
     private Element[][]elements = new Element[12][20];
 
-    public Level(Element[][] elements) {
-        this.elements = elements;
+    public Level(){
     }
 
     public Element[][] getElements() {
