c378937a0507d7456b2f850d6591815110fe5831
CptBaroud <gurvan.seveno@gmail.com>
2018-05-28 14:16:59 +0200

    Add class Element

---
+++ lorann/model/src/main/java/model/Element.java
@@ -0,0 +1,39 @@
+package model;
+
+import java.awt.*;
+
+public class Element {
+
+    protected Image sprite;
+    protected int x;
+    protected int y;
+
+    public Element(int x, int y, Image sprite){
+
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public Image getSprite() {
+        return sprite;
+    }
+
+    public void setSprite(Image sprite) {
+        this.sprite = sprite;
+    }
+
+}
