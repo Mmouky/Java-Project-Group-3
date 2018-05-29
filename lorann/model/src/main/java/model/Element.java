d898cb214383becad73e795d2d914b91b64786b8
Mmouky <33157839+Mmouky@users.noreply.github.com>
2018-05-29 11:26:36 +0200

    toString for Element + getElement in controller

--- lorann/model/src/main/java/model/Element.java
+++ lorann/model/src/main/java/model/Element.java
@@ -4,38 +4,43 @@ import java.awt.*;
 
 public abstract class Element {
 
-    protected Image sprite;
-    protected int x;
-    protected int y;
+	protected Image sprite;
+	protected int x;
+	protected int y;
 
-    public Element(int x, int y, Image sprite){
-        this.y = y;
-        this.x = x;
-        this.sprite = sprite;
-    }
+	public Element(int x, int y, Image sprite) {
+		this.y = y;
+		this.x = x;
+		this.sprite = sprite;
+	}
 
-    public int getX() {
-        return x;
-    }
+	public int getX() {
+		return x;
+	}
 
-    public void setX(int x) {
-        this.x = x;
-    }
+	public void setX(int x) {
+		this.x = x;
+	}
 
-    public int getY() {
-        return y;
-    }
+	public int getY() {
+		return y;
+	}
 
-    public void setY(int y) {
-        this.y = y;
-    }
+	public void setY(int y) {
+		this.y = y;
+	}
 
-    public Image getSprite() {
-        return sprite;
-    }
+	public Image getSprite() {
+		return sprite;
+	}
 
-    public void setSprite(Image sprite) {
-        this.sprite = sprite;
-    }
+	public void setSprite(Image sprite) {
+		this.sprite = sprite;
+	}
+
+	@Override
+	public String toString() {
+		return "Element";
+	}
 
 }
