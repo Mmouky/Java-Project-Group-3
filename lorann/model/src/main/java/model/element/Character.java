0db205193f88fe933d691ca445d18993a2dd37ca
GwNTM <gwenael.griffon@gmail.com>
2018-05-31 16:07:06 +0200

    im dead

--- lorann/model/src/main/java/model/element/Character.java
+++ lorann/model/src/main/java/model/element/Character.java
@@ -7,8 +7,6 @@ import model.Element;
 import model.IElement;
 import model.ILevel;
 import model.IMobile;
-import model.element.characters.ELorann;
-import model.element.characters.Lorann;
 import model.element.door.Door;
 import model.element.door.EDoor;
 import model.element.wall.Wall;
@@ -31,41 +29,24 @@ public abstract class Character extends Element implements IMobile {
 
 		checkCase(xEl, yEl);
 
-		if (this instanceof Lorann) {
-			Lorann lorann = (Lorann) this;
-			lorann.seteLorann(ELorann.UP);
-		}
-
 	}
 
 	public void moveDown() {
 		int yEl = this.getY() + 1;
 		int xEl = this.getX();
 		checkCase(xEl, yEl);
-		if (this instanceof Lorann) {
-			Lorann lorann = (Lorann) this;
-			lorann.seteLorann(ELorann.DOWN);
-		}
 	}
 
 	public void moveLeft() {
 		int yEl = this.getY();
 		int xEl = this.getX() - 1;
 		checkCase(xEl, yEl);
-		if (this instanceof Lorann) {
-			Lorann lorann = (Lorann) this;
-			lorann.seteLorann(ELorann.LEFT);
-		}
 	}
 
 	public void moveRight() {
 		int yEl = this.getY();
 		int xEl = this.getX() + 1;
 		checkCase(xEl, yEl);
-		if (this instanceof Lorann) {
-			Lorann lorann = (Lorann) this;
-			lorann.seteLorann(ELorann.RIGHT);
-		}
 	}
 
 	public void checkCase(int x, int y) {
@@ -74,6 +55,8 @@ public abstract class Character extends Element implements IMobile {
 			this.setX(x);
 			checkEnergyBall(x, y);
 			checkMoney(x, y);
+			checkEnnemy(x, y);
+
 		}
 	}
 
@@ -87,6 +70,20 @@ public abstract class Character extends Element implements IMobile {
 						((EnergyBall) level.getElements()[x][y]).seteBonus(EBonus.DISABLE);
 					}
 				}
+			}
+		}
+	}
+
+	public void checkEnnemy(int x, int y) {
+		for(IMobile iMobile : level.getMonsters()) {
+			if (iMobile.getPosition().equals(new Point(x, y))) {
+				this.die();
+			}
+		}
+
+		if (level.getElements()[x][y] instanceof Door) {
+			if (((Door)level.getElements()[x][y]).geteDoor()== EDoor.CLOSE){
+				this.die();
 			}
 		}
 	}
@@ -118,6 +115,7 @@ public abstract class Character extends Element implements IMobile {
 
 	protected void die() {
 		this.isAlive = false;
+		System.out.println("juimor");
 	}
 
 	@Override
